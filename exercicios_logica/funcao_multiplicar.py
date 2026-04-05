040ce339cfdb18eb5056e8488b1333a8816d2251
Felipe Vieira de Lucena <felipevieiralucena02@gmail.com>
2026-04-05 19:32:27 -0300

    Exercicio 2 - Funcoes e Retornos

---
+++ exercicios_logica/funcao_multiplicar.py
@@ -0,0 +1,8 @@
+# Exercício 02: Entendendo Funções e Retornos
+def multiplicar(a, b):
+    return a * b
+
+# Chamamos a função duas vezes e somamos os resultados: (5*3) + (2*4)
+resultado = multiplicar(5, 3) + multiplicar(2, 4)
+
+print(f"O resultado da conta é: {resultado}")
