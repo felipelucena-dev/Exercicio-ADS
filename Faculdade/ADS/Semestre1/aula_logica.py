e348b569f5a65e0a0810d89775709c30f4cfdd96
Felipe Vieira de Lucena <felipevieiralucena02@gmail.com>
2026-04-06 10:58:59 -0300

    Faxina final: Sincronizando pastas com o GitHub

---
+++ Faculdade/ADS/Semestre1/aula_logica.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+print(f"{n1} + {n2} = {n1 + n2}")
+print(f"{n1} - {n2} = {n1 - n2}")
+print(f"{n1} * {n2} = {n1 * n2}")
+print(f"{n1} / {n2} = {n1 / n2:.2f}") # :.2f formata para 2 casas decimais
+print(f"{n1} // {n2} = {n1 // n2}")
+print(f"{n1} % {n2} = {n1 % n2}")
+print(f"{n1} ** {n2} = {n1 ** n2}")
--- exercicios_logica/funcao_multiplicar.py
+++
@@ -1,8 +0,0 @@
-# Exercício 02: Entendendo Funções e Retornos
-def multiplicar(a, b):
-    return a * b
-
-# Chamamos a função duas vezes e somamos os resultados: (5*3) + (2*4)
-resultado = multiplicar(5, 3) + multiplicar(2, 4)
-
-print(f"O resultado da conta é: {resultado}")
